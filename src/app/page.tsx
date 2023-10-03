e71a5d3b539ae073a2672673ff15b320c311ba61
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-10-03 18:30:17 -0300

    feat: inplementando ultimos dados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,35 @@ import { fadeIn } from "./variants";
 export default function Home() {
   return (
     <motion.div>
-     <div>
-      <h1>Trasforming Ideas <br /> Into <span>Digital Reality</span></h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit veritatis nesciunt cumque ipsam commodi corrupti eius incidunt. Nam voluptates dolorem totam ullam soluta vel? Expedita aut sed nobis neque commodi!</p>
       <div>
-        {/* <ProjectsBtn /> */}
-        <button>Projectsbtn</button>
+        <h1>
+          Trasforming Ideas <br /> Into <span>Digital Reality</span>
+        </h1>
+        <motion.p
+          variants={fadeIn("down", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          veritatis nesciunt cumque ipsam commodi corrupti eius incidunt. Nam
+          voluptates dolorem totam ullam soluta vel? Expedita aut sed nobis
+          neque commodi!
+        </motion.p>
+        <motion.div
+          variants={fadeIn("down", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
+          {/* <ProjectsBtn /> */}
+          <button>Projectsbtn</button>
+        </motion.div>
       </div>
-     </div>
-     <div>image</div>
+      <div>image</div>
+      {/* sem background pra baguia com fundo ele q turou o background*/}
+      {/* mix color css como n sei */}
+      <div>particles</div>
     </motion.div>
   );
 }
