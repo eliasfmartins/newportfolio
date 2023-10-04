b5b9fb1a0fc370489dea8ea0c63c3f3b97820603
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-10-04 19:01:27 -0300

    feat: ajustando styles header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,24 +1,27 @@
 import Link from "next/link";
 import { BsInstagram } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa";
+import { HeaderContainer, HeaderContent } from "./styles";
 export const Header = () => {
   return (
-    <header>
-      <h2>EliasfMartins</h2>
-      <nav>
-        <Link
-          target="_blank"
-          href={`https://www.instagram.com/eliasf.martins/`}
-        >
-          <BsInstagram />
-        </Link>
-        <Link
-          target="_blank"
-          href={`https://www.instagram.com/eliasf.martins/`}
-        >
-          <FaLinkedin />
-        </Link>
-      </nav>
-    </header>
+    <HeaderContainer>
+      <HeaderContent>
+        <h2>EliasfMartins</h2>
+        <nav>
+          <Link
+            target="_blank"
+            href={`https://www.instagram.com/eliasf.martins/`}
+          >
+            <BsInstagram />
+          </Link>
+          <Link
+            target="_blank"
+            href={`https://www.instagram.com/eliasf.martins/`}
+          >
+            <FaLinkedin />
+          </Link>
+        </nav>
+      </HeaderContent>
+    </HeaderContainer>
   );
 };
---
+++ src/app/components/Header/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+margin: 0 auto;
+background: purple;
+`
+export const HeaderContent = styled.div`
+width: 100%;
+max-width: 1200px;
+padding: 0 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  nav{
+    display: flex;
+    gap: 2rem;
+
+  }
+`
