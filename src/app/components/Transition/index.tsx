e4563dfc850add4a3c8abfcb812435a7d4b6d9ba
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-10-08 15:26:28 -0300

    styles: ajustando ajustando trasitions

--- src/app/components/Transition/index.tsx
+++ src/app/components/Transition/index.tsx
@@ -18,15 +18,30 @@ export const Transtition = () => {
   return (
     //essa div motion e aque aparece tem q botar uma cor e tals pra ver 3 cores diferentes
     <div>
-      <motion.div className="first" variants={transitionsVariants} initial="initial" animate="animate" exit="exit" transition={{delay:0.2, duration:0.6, ease:'easeInOut'}}>
-        
-      </motion.div>
-      <motion.div className='second'variants={transitionsVariants} initial="initial" animate="animate" exit="exit" transition={{delay:0.4, duration:0.6, ease:'easeInOut'}}>
-        
-      </motion.div>
-      <motion.div className="terceira" variants={transitionsVariants} initial="initial" animate="animate" exit="exit" transition={{delay:0.6, duration:0.6, ease:'easeInOut'}}>
-        
-      </motion.div>
+      <motion.div
+        style={{ background: red }}
+        variants={transitionsVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+      ></motion.div>
+      <motion.div
+        style={{ background: green }}
+        variants={transitionsVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
+      ></motion.div>
+      <motion.div
+        style={{ background: purple }}
+        variants={transitionsVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
+      ></motion.div>
     </div>
   );
 };
