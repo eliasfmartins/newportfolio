5594222bda6221a33c5c41403ffb2261fd4823f1
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-10-08 15:33:20 -0300

    styles: ajustando ajustando trasitions

--- src/app/components/Transition/index.tsx
+++ src/app/components/Transition/index.tsx
@@ -1,4 +1,5 @@
-import { motion, animate, delay } from "framer-motion";
+import { motion } from "framer-motion";
+import { DivContainer } from "./styles";
 //variants
 const transitionsVariants = {
   initial: {
@@ -17,31 +18,16 @@ const transitionsVariants = {
 export const Transtition = () => {
   return (
     //essa div motion e aque aparece tem q botar uma cor e tals pra ver 3 cores diferentes
-    <div>
-      <motion.div
-        style={{ background: red }}
-        variants={transitionsVariants}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
-      ></motion.div>
-      <motion.div
-        style={{ background: green }}
-        variants={transitionsVariants}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
-      ></motion.div>
-      <motion.div
-        style={{ background: purple }}
-        variants={transitionsVariants}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
-      ></motion.div>
-    </div>
+    <DivContainer>
+      <motion.div className="first" variants={transitionsVariants} initial="initial" animate="animate" exit="exit" transition={{delay:0.2, duration:0.6, ease:'easeInOut'}}>
+        
+      </motion.div>
+      <motion.div className='second'variants={transitionsVariants} initial="initial" animate="animate" exit="exit" transition={{delay:0.4, duration:0.6, ease:'easeInOut'}}>
+        
+      </motion.div>
+      <motion.div className="terceira" variants={transitionsVariants} initial="initial" animate="animate" exit="exit" transition={{delay:0.6, duration:0.6, ease:'easeInOut'}}>
+        
+      </motion.div>
+    </DivContainer>
   );
 };
---
+++ src/app/components/Transition/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const DivContainer = styled.div`
+display: flex;
+.first{
+  background: gray;
+}
+.second{
+  background: pink;
+}
+.terceira{
+  background: purple;
+}
+`
