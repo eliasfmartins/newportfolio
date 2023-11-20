a830c15acb8dc934e49d2ab31b077641d6bd3fef
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-11-20 18:25:20 -0300

    feat: implementando Register no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,39 @@
-"use client";
-import { Sora } from "next/font/google";
-import { Nav } from "./components/Nav";
-import { Header } from "./components/Header";
-import { AnimatePresence, motion } from "framer-motion";
-import { Transtition } from "./components/Transition";
-import { GlobalStyles } from "./globalStyles";
+'use client';
+import { Sora } from 'next/font/google';
+import { Nav } from './components/Nav';
+import { Header } from './components/Header';
+import { AnimatePresence, motion } from 'framer-motion';
+import { Transtition } from './components/Transition';
+import { GlobalStyles } from './globalStyles';
+import StyledComponentsRegistry from './lib/registry';
 
 const sora = Sora({
-  subsets: ["latin"],
-  variable: "--font-sora",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+	subsets: ['latin'],
+	variable: '--font-sora',
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={sora.className}>
-        <AnimatePresence mode="wait">
-          <Transtition />
-          <motion.div>
-            <Nav />
-            <Header />
-            {children}
-          </motion.div>
-        </AnimatePresence>
-        <GlobalStyles />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={sora.className}>
+				<StyledComponentsRegistry>
+
+					<AnimatePresence mode="wait">
+						<Transtition />
+						<motion.div>
+							<Nav />
+							<Header />
+							{children}
+						</motion.div>
+					</AnimatePresence>
+					<GlobalStyles />
+				</StyledComponentsRegistry>
+			</body>
+		</html>
+	);
 }
