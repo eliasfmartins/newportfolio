16490d2b7a82d611255a10cd33b71d8457209498
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-07-25 19:29:23 -0300

    fix style

--- src/app/components/Routatebtn/index.tsx
+++ src/app/components/Routatebtn/index.tsx
@@ -1,42 +1,45 @@
 import { HiArrowDownRight } from 'react-icons/hi2';
 import styled from 'styled-components';
 import Link from 'next/link';
+// import { HiArrowDownRight } from 'react-icons/hi';
 import Image from 'next/image';
 
 export const RoutateBtn = () => {
-  return (
-    <BtnContainer>
-      <Link href={'/work'}>
-        <div className='content'>
-          <Image src={'/rounded-text.png'} width={185} height={185} alt='' className='imagem' />
-          <HiArrowDownRight className='arrow' size={38} />
-        </div>
-      </Link>
-    </BtnContainer>
-  );
+	return (
+		<BtnContainer>
+			<Link href={'/work'}>
+				<div className='content'>
+					<Image src={'/rounded-text.png'} width={185} height={185} alt='' className='imagem' />
+					<HiArrowDownRight className='arrow' size={38} />
+				</div>
+			</Link>
+		</BtnContainer>
+	);
 };
 
 const BtnContainer = styled.div`
-  border-radius: 50%;
-  border: solid white 1px;
+@media (max-width:900px){
+	backdrop-filter: blur(2px); 
+}
+border-radius: 50%;
+border: solid white 1px;
   position: relative;
+  width: 185px;
+  height: 185px;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: center; /* Center horizontally */
+  /* background-image: url('/circle-star.svg'); */
   background-size: cover;
   background-position: center;
+ &:hover{
+	.arrow {
+		/* transform: rotate(-80deg); */
+		color: #FF8C00;
+		margin-left: 15px;
+	}
 
-  @media (max-width: 900px) {
-    backdrop-filter: blur(2px);
-  }
-
-  &:hover {
-    .arrow {
-      color: #ff8c00;
-      margin-left: 15px;
-    }
-  }
-
+ }
   .content {
     display: flex;
     flex-direction: column;
@@ -53,19 +56,21 @@ const BtnContainer = styled.div`
   }
 
   .imagem {
+    margin: auto;
     animation: rotate 6s linear infinite;
     transform-origin: center center;
-		height: 100%;
-		width: 100%;
+width: 141px;
+ height: 148px;
   }
 
   .arrow {
     position: absolute;
-    transition: 0.5s;
-    font-weight: 700;
-    top: 40%;
+		transition: 0.5s;
+		font-weight: 700;
+		top: 40%;
     font-size: 2rem;
-    color: white;
+		color: white;
     transform: rotate(-50deg);
   }
+	
 `;
